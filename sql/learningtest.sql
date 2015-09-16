562f352d10fbb18d3fca0cf5679eecad0c0c35f7
Mikko Rinne <mikko.rinne@aalto.fi>
2015-09-16 12:17:24 +0300

    Updates to learningtest.sql.

--- sql/learningtest.sql
+++ sql/learningtest.sql
@@ -1,10 +1,34 @@
-DELETE FROM device_data
-WHERE id IN (
-  SELECT id
-  FROM (
-    SELECT id, row_number() OVER point AS row
-    FROM device_data
-    WINDOW point AS (PARTITION BY device_id, time)
-  ) AS grouped_points
-  WHERE row > 1
+
+DROP TABLE IF EXISTS averaged_location;
+
+CREATE TABLE averaged_location (
+  -- Comment
+  device_id integer,
+  time_stamp integer,
+  minute integer,
+  hour integer,
+  year integer,
+  day_of_week integer,
+  day_of_year integer,
+  longitude FLOAT,
+  latitude FLOAT,
+  accuracy FLOAT,
+  waypoint_id bigint
 );
+
+INSERT INTO averaged_location
+      SELECT
+        device_id,
+	CAST(time as INT) as time_stamp,
+	CAST(date_part('minute',time) as integer) as minute,
+	CAST(date_part('hour',time) as integer) as hour,
+	CAST(date_part('year',time) as integer) as year,
+	CAST(date_part('dow',time) as integer) as day_of_week,
+	CAST(date_part('doy',time) as integer) as day_of_year,
+        ST_Y(coordinate::geometry) as longitude,
+        ST_X(coordinate::geometry) as latitude,
+        accuracy,
+        waypoint_id
+      FROM device_data
+      ORDER BY time ASC
+;
