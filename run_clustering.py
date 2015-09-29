56955d70ad1722b4c3cf6ae8ec5b02fa5790f3f6
Jesse Read <jesse.read@gmail.com>
2015-09-29 15:41:11 +0300

    Add clustering script

---
+++ run_clustering.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2.4
+#
+# Small script to show PostgreSQL and Pyscopg together
+#
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='regularroutes' user='regularroutes' host='localhost' password='TdwqStUh5ptMLeNl' port=5432")
+except:
+    print "I am unable to connect to the database"
+
+
+cur = conn.cursor()
+
+print "Loading Device IDs ..."
+ids = array(c.execute('SELECT UNIQUE device_id FROM data_averaged').fetchall(),dtype={'names':['d_id'], 'formats':['i4']})
+
+for i in ids:
+    print "Loading Waypoints for Device ID ", i, "..."
+    dat = array(c.execute('SELECT longitude,latitude,time_stamp,device_id FROM data_averaged WHERE device_id=?', (i,)).fetchall(),dtype={'names':['lon', 'lat', 't', 'd_id'], 'formats':['f4','f4','f4','i4']})
+    X = column_stack([dat['lat'],dat['lon']])
+    
+    if len(dat) < 50: 
+        print "Not enough points for Clustering!"
+
+    else:
+        print "Clustering ..."
+        from sklearn.cluster import KMeans
+        h = KMeans(50, max_iter=100, n_init=1)
+        h.fit(X)
+        labels = h.labels_
+
+        print "Inserting Cluster Centres ..."
+        for k in labels:
+            sql = "INSERT INTO cluster_centers (longitude, latitude, device_id) VALUES (%s, %s, %s)"
+            cur.execute(sql, k[0], k[1], i)
+
+        print "Commit"
+        conn.commit()
+
+print "Done"
