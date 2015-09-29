f323cfdfa67f2e3ca7b008682a005c21b2de0543
Jesse Read <jesse.read@gmail.com>
2015-09-29 15:25:13 +0300

    added test file

---
+++ test_psycopg2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.4
+#
+# Small script to show PostgreSQL and Pyscopg together
+#
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='regularroutes' user='regularroutes' host='localhost' password='TdwqStUh5ptMLeNl' port=5432")
+except:
+    print "I am unable to connect to the database"
+
+
+cur = conn.cursor()
+try:
+    cur.execute("""SELECT * from device_data""")
+except:
+    print "I can't SELECT from device_data"
+
+rows = cur.fetchall()
+print "\nRows: \n"
+for row in rows:
+    print "   ", row
