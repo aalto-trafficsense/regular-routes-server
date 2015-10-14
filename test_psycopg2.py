5c0af7f8b1297b9106a5f5f5f98873bbda1e5ba4
Jesse Read <jesse.read@gmail.com>
2015-10-14 14:25:56 +0300

    added a method 'dev_ids()'

--- test_psycopg2.py
+++ test_psycopg2.py
@@ -5,19 +5,24 @@
 
 import psycopg2
 
-try:
-    conn = psycopg2.connect("dbname='regularroutes' user='regularroutes' host='localhost' password='TdwqStUh5ptMLeNl' port=5432")
-except:
-    print "I am unable to connect to the database"
+def dev_ids():
+	try:
+	    conn = psycopg2.connect("dbname='regularroutes' user='regularroutes' host='localhost' password='TdwqStUh5ptMLeNl' port=5432")
+	except:
+	    return "I am unable to connect to the database"
 
 
-cur = conn.cursor()
-try:
-    cur.execute("""SELECT DISTINCT device_id from device_data LIMIT 10""")
-except:
-    print "I can't SELECT from device_data"
+	cur = conn.cursor()
+	try:
+	    cur.execute("""SELECT DISTINCT device_id from device_data LIMIT 10""")
+	except:
+	    return "I can't SELECT from device_data"
 
-rows = cur.fetchall()
-print "\nRows: \n"
-for row in rows:
-    print "   ", row
+	rows = cur.fetchall()
+	#print "\nRows: \n"
+	#for row in rows:
+	#    print "   ", row
+
+	return "DEVICE IDs (limit 10): "+str(rows)
+
+print dev_ids()
