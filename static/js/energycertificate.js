9f77668f4702715012d4411592f7ac7368b9012b
Tuukka Tolvanen <tuukka.tolvanen@aalto.fi>
2017-02-15 18:41:32 +0200

    Split date range picker from energycertificate

--- static/js/energycertificate.js
+++ static/js/energycertificate.js
@@ -1,50 +1,10 @@
 $(document).ready(function() {
 
-    var firstpicker = $('#firstday');
-    firstpicker.pickadate({
-	firstDay: 1,
-	format: 'dd.mm.yyyy',
-	hiddenName: true,
-	onClose: function() {
-            var dates = location.hash.split("#").splice(-1)[0].split("/");
-            dates[0] = this.get('select', 'yyyy-mm-dd');
-            dates[1] = dates[1] || "";
-            location.hash = dates.join("") && dates.join("/") || "";
-	    firstpicker.blur();
-	},
-    });
-
-    var lastpicker = $('#lastday');
-    lastpicker.pickadate({
-	firstDay: 1,
-	format: 'dd.mm.yyyy',
-	hiddenName: true,
-	onClose: function() {
-            var dates = location.hash.split("#").splice(-1)[0].split("/");
-            dates[0] = dates[0] || "";
-            dates[1] = this.get('select', 'yyyy-mm-dd');
-            location.hash = dates.join("") && dates.join("/") || "";
-	    lastpicker.blur();
-	},
-    });
-
     function hashchange() {
         var hash = location.hash.split("#").splice(-1)[0];
         var dates = hash.split("/");
         var firstday = dates[0];
         var lastday = dates[1];
-
-        if (firstday)
-            $("#firstday").pickadate("picker").set(
-                "select", Date.parse(firstday));
-        else
-            $("#firstday").pickadate("picker").clear();
-
-        if (lastday)
-            $("#lastday").pickadate("picker").set(
-                "select", Date.parse(lastday));
-        else
-            $("#lastday").pickadate("picker").clear();
 
         var url = "energycertificate_svg";
         var args = [];
@@ -56,33 +16,6 @@ $(document).ready(function() {
         if (args)
             url += "?" + args;
         $("#svg").attr("src", url);
-
-        var yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 1);
-        yesterday = yesterday.toISOString().slice(0, 10);
-
-        var rweek = new Date();
-        rweek.setDate(rweek.getDate() - 7);
-        rweek = rweek.toISOString().slice(0, 10) + "/" + yesterday;
-
-        var rmonth = new Date();
-        rmonth.setMonth(rmonth.getMonth() - 1);
-        rmonth = rmonth.toISOString().slice(0, 10) + "/" + yesterday;
-
-        var ryear = new Date();
-        ryear.setFullYear(ryear.getFullYear() - 1);
-        ryear = ryear.toISOString().slice(0, 10) + "/" + yesterday;
-
-        var presets = [
-            {id: "#weeklink", href: rweek},
-            {id: "#monthlink", href: rmonth},
-            {id: "#yearlink", href: ryear}];
-
-        for (var i = 0; i < presets.length; ++i)
-            if (hash == presets[i].href)
-                $(presets[i].id).removeAttr("href")
-            else
-                $(presets[i].id).attr("href", "#" + presets[i].href);
     }
 
     $(window).on("hashchange", hashchange);
